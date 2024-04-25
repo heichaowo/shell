74dccd91c55cac51aeb41a56a2e6e2b4b7338d7b
heichaowo <index.erlic@gmail.com>
2024-04-25 20:22:35 +0800

    Update adapt.sh

--- firewall/adapt.sh
+++ firewall/adapt.sh
@@ -4,6 +4,7 @@ iptables -P INPUT ACCEPT
 #Allow several ports
 iptables -A INPUT -p tcp --dport 4869 -j ACCEPT
 iptables -A INPUT -p tcp --dport 10000 -j ACCEPT
+iptables -A INPUT -p tcp --dport 10001 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8008 -j ACCEPT #Nezha
 iptables -A INPUT -p tcp --dport 5555 -j ACCEPT #Nezha
 

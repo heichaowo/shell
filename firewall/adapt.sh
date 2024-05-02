e4ba42b50b8036db51a95f0eba5d609d92484d25
heichaowo <index.erlic@gmail.com>
2024-05-02 16:14:55 +0800

    Update adapt.sh

--- firewall/adapt.sh
+++ firewall/adapt.sh
@@ -1,8 +1,8 @@
 #Allow several ports
-iptables -A INPUT -p tcp --dport 4869 -j ACCEPT
-iptables -A INPUT -p tcp --dport 10000 -j ACCEPT
-iptables -A INPUT -p tcp --dport 51821 -j ACCEPT
-iptables -A INPUT -p udp --dport 51820 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8443 -j ACCEPT
+#iptables -A INPUT -p tcp --dport 10000 -j ACCEPT
+#iptables -A INPUT -p tcp --dport 51821 -j ACCEPT
+#iptables -A INPUT -p udp --dport 51820 -j ACCEPT
 
 # Save the rules
 netfilter-persistent save

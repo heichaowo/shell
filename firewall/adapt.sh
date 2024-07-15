7605c23d6487ad9f7b86818894b7040baf8b4baa
heichaowo <index.erlic@gmail.com>
2024-07-15 22:54:43 +0800

    Update and rename adapt.sh to adapt1.sh

--- firewall/adapt.sh
+++
@@ -1,9 +0,0 @@
-#Allow several ports
-iptables -A INPUT -p tcp --dport 8443 -j ACCEPT
-iptables -A INPUT -p udp --dport 8443 -j ACCEPT
-#iptables -A INPUT -p tcp --dport 10000 -j ACCEPT
-#iptables -A INPUT -p tcp --dport 51821 -j ACCEPT
-#iptables -A INPUT -p udp --dport 51820 -j ACCEPT
-
-# Save the rules
-netfilter-persistent save

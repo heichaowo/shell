8c19d4b68fcc90b24a31d5ac1a808f2dfcc9fe60
heichaowo <index.erlic@gmail.com>
2024-09-07 03:31:59 +0800

    支持SAN证书申请

--- ops/oh-my-acme.sh
+++ ops/oh-my-acme.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# 确保域名被传递
+# 确保至少一个域名被传递
 if [ -z "$1" ]; then
-    echo "请提供域名。用法: $0 example.com"
+    echo "请提供至少一个域名。用法: $0 example.com [additional-domains...]"
     exit 1
 fi
 
+# 读取主域名和其他附加域名
 DOMAIN=$1
+shift  # 移除第一个参数，后面的都作为附加域名
+SAN_DOMAINS="$@"
+
 WEBROOT="/var/www/html"  # 设置为你的Web服务器的根目录路径
 KEY_DIR="/etc/ssl/$DOMAIN"
 ACME_PATH="$HOME/.acme.sh"
@@ -65,6 +69,12 @@ if [ "$CA_SERVER" = "buypass" ] || [ "$CA_SERVER" = "zerossl" ]; then
     $ACME_PATH/acme.sh --register-account -m "${RANDOM}@dedsec.cc" --server $CA_SERVER --force --insecure
 fi
 
+# 准备 SAN 域名的参数
+SAN_ARGS=""
+for san in $SAN_DOMAINS; do
+    SAN_ARGS="$SAN_ARGS -d $san"
+done
+
 # 使用standalone模式申请SSL证书
 if [ "$MODE_OPTION" = "1" ]; then
     # 检查并添加iptables规则以打开端口（如果规则不存在）
@@ -77,7 +87,7 @@ if [ "$MODE_OPTION" = "1" ]; then
         IPTABLES_RULE_ADDED=false
     fi
 
-    $ACME_PATH/acme.sh --issue -d $DOMAIN --standalone -k ec-256 --force --insecure --httpport $CUSTOM_PORT --server $CA_SERVER
+    $ACME_PATH/acme.sh --issue -d $DOMAIN $SAN_ARGS --standalone -k ec-256 --force --insecure --httpport $CUSTOM_PORT --server $CA_SERVER
 
     # 如果脚本中添加了iptables规则，则删除该规则
     if [ "$IPTABLES_RULE_ADDED" = true ]; then
@@ -87,7 +97,7 @@ if [ "$MODE_OPTION" = "1" ]; then
 
 # 使用webroot模式申请SSL证书（默认）
 elif [ "$MODE_OPTION" = "2" ]; then
-    $ACME_PATH/acme.sh --issue -d $DOMAIN -w $WEBROOT -k ec-256 --force --insecure --server $CA_SERVER
+    $ACME_PATH/acme.sh --issue -d $DOMAIN $SAN_ARGS -w $WEBROOT -k ec-256 --force --insecure --server $CA_SERVER
 else
     echo "无效的选项。请选择1或2。"
     exit 1
@@ -104,4 +114,4 @@ $ACME_PATH/acme.sh --install-cert -d $DOMAIN --ecc \
     --key-file $KEY_DIR/server.key \
     --fullchain-file $KEY_DIR/server.crt
 
-echo "SSL证书已成功为 $DOMAIN 申请并安装到 $KEY_DIR 中"
+echo "SSL证书已成功为 $DOMAIN 及其SAN域名申请并安装到 $KEY_DIR 中"

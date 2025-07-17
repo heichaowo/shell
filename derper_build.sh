097f585ee4965a2b857f30a841040b0cb9fc00a0
heichaowo <index.erlic@gmail.com>
2025-07-17 11:50:49 +0800

    Update derper_build.sh

--- derper_build.sh
+++ derper_build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+wget https://go.dev/dl/go1.24.5.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.5.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version

e808e85210060a4b3f492994cbff6201bb75852a
heichaowo <index.erlic@gmail.com>
2026-05-13 12:56:59 +0800

    Update and rename bbr+fq_codel.sh to bbr3.sh

--- bbr+fq_codel.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#启用BBR+fq_codel
-  remove_bbr_lotserver
-  echo "net.core.default_qdisc=fq_codel" >>/etc/sysctl.d/99-sysctl.conf
-  echo "net.ipv4.tcp_congestion_control=bbr" >>/etc/sysctl.d/99-sysctl.conf
-  sysctl --system
-  echo -e "${Info}BBR+FQ_CODEL修改成功，重启生效！"

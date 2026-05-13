a9c17003d23be87d9f3690d42b3d816ed9dd4375
heichaowo <index.erlic@gmail.com>
2026-05-13 12:56:58 +0800

    Update update_xray.sh

--- ops/update_xray.sh
+++ ops/update_xray.sh
@@ -1,35 +1,49 @@
 #!/bin/bash
 
-# Define the target and temporary download locations
-TARGET="/usr/local/x-ui/bin/xray-linux-amd64"
-TEMP_DOWNLOAD="/tmp/xray-linux-64.zip"
+# GitHub 仓库页面
+repo_url="https://github.com/XTLS/Xray-core/releases"
 
-# Download the latest version
-echo "Downloading the latest Xray version..."
-wget -qO $TEMP_DOWNLOAD https://github.com/XTLS/Xray-core/releases/download/latest/Xray-linux-64.zip
+# 获取最新版本号，标记为 'Latest'
+# 根据提供的 HTML 片段更新正则表达式
+version=$(curl -s $repo_url | grep -oP '(?<=href="/XTLS/Xray-core/releases/tag/)\w+\.\d+\.\d+' | head -n1)
 
-# Check if the download was successful
-if [ $? -ne 0 ]; then
-    echo "Failed to download the file. Exiting..."
-    exit 1
+# 检查是否成功获取版本号
+if [ -z "$version" ]; then
+  echo "Failed to find the latest version number."
+  exit 1
 fi
 
-# Unzip the new version
-echo "Unpacking the new version..."
-unzip -o $TEMP_DOWNLOAD -d /tmp
+# 构建完整的下载链接
+full_url="https://github.com/XTLS/Xray-core/releases/download/${version}/Xray-linux-64.zip"
 
-# Replace the old executable with the new one
-echo "Updating the executable..."
-cp -f /tmp/xray-linux-64 $TARGET
+# 下载最新的 zip 文件
+wget $full_url -O /tmp/Xray-linux-64.zip
 
-# Ensure the new executable has the correct permissions
-chmod +x $TARGET
+# 检查是否下载成功
+if [ $? -ne 0 ]; then
+  echo "Failed to download the file."
+  exit 1
+fi
 
-# Restart the x-ui service
-echo "Restarting the x-ui service..."
-systemctl restart x-ui
+# 停止 x-ui 服务以确保可以替换文件
+systemctl stop x-ui
 
-# Cleanup
-rm -f $TEMP_DOWNLOAD
-echo "Update completed successfully!"
+# 解压 zip 文件直接到目标目录，自动覆盖已存在的文件
+unzip -o /tmp/Xray-linux-64.zip -d /usr/local/x-ui/bin/
 
+# 检查解压是否成功
+if [ $? -ne 0 ]; then
+  echo "Failed to unzip the file."
+  exit 1
+fi
+
+# 重命名 xray 为 xray-linux-amd64
+mv /usr/local/x-ui/bin/xray /usr/local/x-ui/bin/xray-linux-amd64
+
+# 重启 x-ui 服务
+systemctl start x-ui
+
+# 清理临时文件
+rm -rf /tmp/Xray-linux-64.zip
+
+echo "Xray has been updated and x-ui service restarted."

bd9cbb275f0f9644a36352b8b3b230622e84fc88
heichaowo <index.erlic@gmail.com>
2026-05-13 12:56:57 +0800

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -6,21 +6,34 @@ apt install iptables-persistent
 # Flush existing rules and set default policies
 iptables -F
 iptables -X
-iptables -P OUTPUT ACCEPT
 
 # Allow loopback traffic
 iptables -A INPUT -i lo -j ACCEPT
 
-# Allow SSH, HTTP, and HTTPS
+# Allow SSH, HTTP, and HTTPS and Cloudflare HTTPS port
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 2053 -j ACCEPT
+iptables -A INPUT -p tcp --dport 2083 -j ACCEPT
+iptables -A INPUT -p tcp --dport 2087 -j ACCEPT
+iptables -A INPUT -p tcp --dport 2096 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8443 -j ACCEPT
+
+# Allow UDP 443
+iptables -A INPUT -p udp --dport 443 -j ACCEPT
 
 # Allow a range of UDP ports (20000-50000)
 iptables -A INPUT -p udp --dport 20000:50000 -j ACCEPT
 
 # Allow related and established connections
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+# Deny other input
+iptables -P INPUT DROP
+
+# Allow all output 
+iptables -P OUTPUT ACCEPT
 
 # NAT rule for UDP port redirection
 iptables -t nat -A PREROUTING -p udp --dport 20000:50000 -j DNAT --to-destination :443
